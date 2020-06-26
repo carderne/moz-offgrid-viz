c9e4a4d1db59c8b15bf80466ce9829d052279106
Chris Arderne <chris@rdrn.me>
2020-06-26 16:04:03 +0200

    Add layer toggle, style tweaks

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiY2FyZGVybmUiLCJhIjoiY2p4ZWd2MmZ1MGtvazNwbnpmbGJqbHhjeiJ9.dlwfb8JCUTb3Y2-8Lgcsng';
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2FyZGVybmUiLCJhIjoiY2puMXN5cnBtNG53NDN2bnhlZ3h4b3RqcCJ9.eNjrtezXwvM7Ho1VSxo06w';
 let maxBounds = [
   [15, -35],
   [55, -5]
@@ -12,16 +12,29 @@ let map = new mapboxgl.Map({
   minZoom: 5,
   maxBounds: maxBounds
 });
-map.setLayoutProperty('mapbox-satellite', 'visibility', 'none');
+
+map.on('load', () => {
+  map.addControl(new mapboxgl.NavigationControl());
+});
 
 const get = document.getElementById.bind(document);
 const query = document.querySelector.bind(document);
 
 let modalRoot = get('modal-root');
 let modal = query('.modal');
+let toggleSatellite = get('toggle-satellite');
 
 modalRoot.addEventListener('click', rootClick);
 modal.addEventListener('click', modalClick);
+toggleSatellite.addEventListener('change', toggleLayer);
+
+function toggleLayer(e) {
+  toggleMap = {
+    'toggle-satellite': 'mapbox-satellite'
+  }
+  vis = e.target.checked ? 'visible' : 'none';
+  map.setLayoutProperty(toggleMap[e.target.id], 'visibility', vis);
+}
 
 function rootClick() {
   modalRoot.classList.remove('visible');
