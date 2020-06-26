ca052a33498351a944ada4c8529952b373c1ea55
Chris Arderne <chris@rdrn.me>
2020-06-26 12:59:26 +0200

    Basic website framework

---
+++ main.js
@@ -0,0 +1,37 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2FyZGVybmUiLCJhIjoiY2p4ZWd2MmZ1MGtvazNwbnpmbGJqbHhjeiJ9.dlwfb8JCUTb3Y2-8Lgcsng';
+let maxBounds = [
+  [15, -35],
+  [55, -5]
+];
+let map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/carderne/ckbw1g00f0p0r1imlb641eur4',
+  center: [35, -18],
+  zoom: 5,
+  maxZoom: 14,
+  minZoom: 4,
+  maxBounds: maxBounds
+});
+
+const get = document.getElementById.bind(document);
+const query = document.querySelector.bind(document);
+
+let modalRoot = get('modal-root');
+let modal = query('.modal');
+
+modalRoot.addEventListener('click', rootClick);
+modal.addEventListener('click', modalClick);
+
+function rootClick() {
+  modalRoot.classList.remove('visible');
+}
+
+function openModal() {
+  modalRoot.classList.add('visible');
+}
+
+function modalClick(e) {
+  e.stopPropagation();
+  e.stopImmediatePropagation();
+  return false;
+}
