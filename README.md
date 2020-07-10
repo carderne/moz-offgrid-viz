3fcdb236156bac3340cb49133892b1e9648def3d
Chris Arderne <chris@rdrn.me>
2020-07-10 16:32:44 +0200

    Functional sliders

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # moz-offgrid-viz
 
 Remove `?fresh=true` from map style URL before publishing.
+
+## Convert GeoJSON to MBtiles
+
+For clusters:
+```
+tippecanoe -z16 -Z5 -o clusters.mbtiles --drop-densest-as-needed clusters.geojson
+```
--- main.js
+++ main.js
@@ -5,27 +5,35 @@ const query = document.querySelector.bind(document);
 
 let modalRoot = get("modal-root");
 let modal = query(".modal");
-let toggleClusters = get("toggle-clusters");
-let toggleSatellite = get("toggle-satellite");
 let about = get("about");
+let toggles = {
+  clusters: get("toggle-clusters"),
+  satellite: get("toggle-satellite"),
+};
+
+about.onclick = openModal;
+modalRoot.onclick = rootClick;
+modal.onclick = modalClick;
 
 mapboxgl.accessToken =
   "pk.eyJ1IjoiY2FyZGVybmUiLCJhIjoiY2puMXN5cnBtNG53NDN2bnhlZ3h4b3RqcCJ9.eNjrtezXwvM7Ho1VSxo06w";
-let maxBounds = [
-  [15, -35],
-  [55, -5],
-];
 let map = new mapboxgl.Map({
   container: "map",
   style: "mapbox://styles/carderne/ckbw6t28y0a4e1iqqzk28d5vd?fresh=true",
   center: [35, -18],
-  zoom: 7,
-  maxZoom: 14,
+  zoom: 5,
+  maxZoom: 16,
   minZoom: 5,
-  maxBounds: maxBounds,
+  maxBounds: [
+    [15, -35],
+    [55, -5],
+  ],
 });
 
+// Any code that needs to interact with map elements must only
+// be activated once the Mapbox map has loaded.
 map.on("load", () => {
+  // Scale and navigation controls
   map.addControl(
     new mapboxgl.ScaleControl({
       maxWidth: 200,
@@ -35,11 +43,10 @@ map.on("load", () => {
   );
   map.addControl(new mapboxgl.NavigationControl(), "top-right");
 
-  modalRoot.onclick = rootClick;
-  modal.onclick = modalClick;
-  toggleClusters.onchange = toggleLayer;
-  toggleSatellite.onchange = toggleLayer;
-  about.onclick = openModal;
+  // Layer toggles
+  for (let key in toggles) {
+    toggles[key].onchange = toggleLayer;
+  }
 
   // Style cursor when entering/leaving clusters
   map.on("mouseenter", "clusters-poly", () => {
@@ -49,41 +56,58 @@ map.on("load", () => {
     map.getCanvas().style.cursor = "";
   });
 
+  // Open info box when click on a cluster
+  // And close info box when click anywhere else
   map.on("click", "clusters-poly", showClusterInfo);
   map.on("click", (e) => {
     if (e.defaultPrevented === false) {
       query(".cluster").style.display = "none";
     }
   });
+
+  // Call function when range slider value changes
+  get("range-grid").oninput = filterUpdate;
 });
+
+function filterUpdate(e) {
+  let id = e.target.id.split("-")[1];
+  let val = parseFloat(e.target.value);
+  console.log(id, val);
+
+  let filters = ["all", [">=", id, val]];
+
+  map.setFilter("clusters-poly", filters);
+  map.setFilter("clusters-line", filters);
+}
 
 function showClusterInfo(e) {
   e.preventDefault();
   query(".cluster").style.display = "block";
 
   let props = e.features[0].properties;
+  console.log(props);
   props = {
     id: props.fid,
-    village: props.village_name,
-    adm3: props.adm3_posto,
-    adm2: props.adm2_district,
-    adm1: props.adm1_province,
+    village: props.village,
+    adm3: props.adm3,
+    adm2: props.adm2,
+    adm1: props.adm1,
     pop: props.pop.toFixed(0),
-    hh: props.households.toFixed(0),
-    popd: props.pop_density.toFixed(0),
+    hh: props.hh.toFixed(0),
+    popd: props.popd.toFixed(0),
     area: props.area.toFixed(2),
     ntl: props.ntl.toFixed(2),
     gdp: props.gdp.toFixed(2),
-    grid: props.gridfinder.toFixed(2),
-    elec: props.electrified,
+    grid: props.grid.toFixed(2),
+    elec: props.elec,
     travel: props.travel.toFixed(0),
-    city: props.nearest_city,
-    cityd: props.city_dist.toFixed(0),
-    urban: props.urban_level.toFixed(0),
-    health: props.health_sites,
-    school: props.schools,
+    city: props.city,
+    cityd: props.cityd.toFixed(0),
+    urban: props.urban.toFixed(0),
+    health: props.health,
+    school: props.school,
     lat: props.lat,
-    lng: props.long,
+    lng: props.lng,
   };
 
   for (let key in props) {
